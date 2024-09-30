d462b8306c04b4b38ed10d8db75abcf929a3b60c
Syeda Samana Zehra <174864609+sy518@users.noreply.github.com>
2024-10-01 01:46:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+//Alert Chapter
+//Q no 1
+//answer
+//The alert() function in JavaScript is used to display a message box on the user's screen. It takes a string as an argument, 
+//which is the message to be displayed.
+
+//Q no 2
+alert("Well come to my website");
+
+//Q no 3
+//What happens if you capitalize the alert keyword?
+//alert isn't capitalized. If you capitalize it, the script will stop.
+
+//Q no 4
+alert("Hello World!");
+
+//Q no 5
+alert("My name is Samana");
+//alert: is a key word, that displays a message box on the user's screen.
+//("My name is Samana") :it represents the message that will be displayed in the alert box.
+//The string can contain any text, including HTML tags.
+
+//Variables for Strings
+//Q no 1
+var message = "Hello, world!"
+alert(message);
+
+//Q no 2
+var age = 25
+alert(age);
+
+//Q no 3
+var greeting = "Good morning!"
+alert(greeting);
+greeting = "Good evening!"
+alert(greeting);
+
+//Q no 4
+var favoriteColor = "blue"
+alert("My favorite color is" + " " + favoriteColor);
+
+
+//Q no 5
+var age;
+age = 25;
+alert(age);
+
+//Variables for Numbers
+//Q no 1
+var height = 170;
+var  newHeight = height + 10;
+alert(newHeight);
+
+//Q no 2
+var num1 = 50;
+var num2 = 30;
+var sum = num1 + num2;
+alert(sum);
+
+//Q no 3
+var num1 = 50;
+var num2 = 30;
+var sum = num1 + num2;
+alert(sum);
+var numToBeAdded = 20;
+var total = sum + numToBeAdded;
+alert(total);
+
+//Q no 4
+var price = 120;
+var taxRate = 0.1; // 10% as a decimal
+
+var taxAmount = price * taxRate;
+var totalPrice = price + taxAmount;
+
+alert("Total price: " + totalPrice);
+
+//Variables for Numbers
+// Q no 1
+var  myAddress = "Karachi"
+alert(myAddress);
+
+//Q no 2
+var userName = "Syeda Samana Zehra"
+var userEmai = "Samanafaraz"
+var userPassword = "***********"
+alert("Username :" + " " + userName + "\nEmail :" + " " +  userEmai + "\nPassword :" + " " +  userPassword);
+
+//Q no 3
+//var user address =  "abcd"  Error
+//alert(user address);  Error
+var userAddress = "xyz"
+alert(userAddress);
+
+//Q no 4
+var rose = "Floribundas"
+//alert(Rose);  //Error
+alert(rose);
